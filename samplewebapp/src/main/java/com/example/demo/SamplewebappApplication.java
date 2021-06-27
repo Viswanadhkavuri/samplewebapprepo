1b5137abe3328de2e0f81db7f443059b57f27ff4
Visuk <Visuk@DESKTOP-QA43UJ1.mshome.net>
2021-06-27 05:00:03 -0500

    first commit

---
+++ samplewebapp/src/main/java/com/example/demo/SamplewebappApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplewebappApplication {
+
+	public static void main(String[] args) {
+		System.out.println("change1");
+		SpringApplication.run(SamplewebappApplication.class, args);
+	}
+
+}
